9cd0cf9f20d63a7020931f61bae00676f7327d39
Dtroupe18 <dave.troupe@gmail.com>
2020-02-14 14:39:14 -0500

    initial commit

---
+++ App.js
@@ -0,0 +1,62 @@
+import React from 'react';
+import { StyleSheet, Text, View, Dimensions } from 'react-native';
+import { LineChart } from "react-native-chart-kit";
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Bezier Line Chart</Text>
+      <LineChart
+        data={{
+          labels: ["January", "February", "March", "April", "May", "June"],
+          datasets: [
+            {
+              data: [
+                Math.random() * 100,
+                Math.random() * 100,
+                Math.random() * 100,
+                Math.random() * 100,
+                Math.random() * 100,
+                Math.random() * 100
+              ]
+            }
+          ]
+        }}
+        width={Dimensions.get("window").width - 16} // from react-native
+        height={220}
+        yAxisLabel="$"
+        yAxisSuffix="k"
+        chartConfig={{
+          backgroundColor: "#e26a00",
+          backgroundGradientFrom: "#fb8c00",
+          backgroundGradientTo: "#ffa726",
+          decimalPlaces: 2, // optional, defaults to 2dp
+          color: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+          labelColor: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+          style: {
+            borderRadius: 16
+          },
+          propsForDots: {
+            r: "6",
+            strokeWidth: "2",
+            stroke: "#ffa726"
+          }
+        }}
+        bezier
+        style={{
+          marginVertical: 8,
+          borderRadius: 16
+        }}
+      />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
